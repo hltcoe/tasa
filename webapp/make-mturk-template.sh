80cafdb1f6373813a99d29c03f1c72d0d0fb6106
Craig Harman <craig@craigharman.net>
2019-11-11 09:49:40 -0500

    Updated NPM dependencies to latest versions. Update template-generation scripts.

--- webapp/make-mturk-template.sh
+++ webapp/make-mturk-template.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-#npm run-script build
 
-# include jquery
-#echo "<script type=\"text/javascript\" src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js\"></script>" > anno-app-mturk.html
-# copy template
-#echo "<script type=\"text/javascript\" src=\"https://hltcoe.github.io/concrete-js/thrift.js\"></script>" >> anno-app-mturk.html
-#echo "<script type=\"text/javascript\" src=\"https://hltcoe.github.io/concrete-js/concrete.js\"></script>" >> anno-app-mturk.html
 echo "<style type=\"text/css\">" > anno-app-mturk.html
 cat build/static/css/main.*.css >> anno-app-mturk.html
 echo "</style>" >> anno-app-mturk.html
 
 cat skeleton.html >> anno-app-mturk.html
+
 ## cat the scripts into the html
 echo "<script type=\"text/javascript\">" >> anno-app-mturk.html
-cat build/static/js/1.*.js >> anno-app-mturk.html
+cat build/static/js/2.*.js >> anno-app-mturk.html
 echo "</script>" >> anno-app-mturk.html
 echo "" >> anno-app-mturk.html
+
 echo "<script type=\"text/javascript\">" >> anno-app-mturk.html
 cat build/static/js/main.*.js >> anno-app-mturk.html
 echo "</script>" >> anno-app-mturk.html
-#
+echo "" >> anno-app-turkle.html
+
+echo "<script type=\"text/javascript\">" >> anno-app-turkle.html
+cat build/static/js/runtime-main.*.js >> anno-app-turkle.html
+echo "</script>" >> anno-app-turkle.html
+
 echo "DONE"
