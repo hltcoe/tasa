0d3b108ff66dc478cac9237cff744db8aaa45b9d
esteng <elias.stengel@gmail.com>
2022-06-06 21:02:49 -0400

    fix interface issue

--- webapp/make-turkle-template.sh
+++ webapp/make-turkle-template.sh
@@ -6,18 +6,18 @@ npm run-script build
 # copy template
 #echo "<script type=\"text/javascript\" src=\"https://hltcoe.github.io/concrete-js/thrift.js\"></script>" >> anno-app-turkle.html
 #echo "<script type=\"text/javascript\" src=\"https://hltcoe.github.io/concrete-js/concrete.js\"></script>" >> anno-app-turkle.html
-echo "<style type=\"text/css\">" > anno-app-turkle.html
-cat build/static/css/main.*.css >> anno-app-turkle.html
-echo "</style>" >> anno-app-turkle.html
+# echo "<style type=\"text/css\">" > anno-app-turkle.html
+# cat build/static/css/main.*.css >> anno-app-turkle.html
+# echo "</style>" >> anno-app-turkle.html
 
 cat skeleton.html >> anno-app-turkle.html
 ## cat the scripts into the html
 echo "<script type=\"text/javascript\">" >> anno-app-turkle.html
-cat build/static/js/2.*.js >> anno-app-turkle.html
-echo "</script>" >> anno-app-turkle.html
-echo "" >> anno-app-turkle.html
-echo "<script type=\"text/javascript\">" >> anno-app-turkle.html
 cat build/static/js/main.*.js >> anno-app-turkle.html
 echo "</script>" >> anno-app-turkle.html
+#echo "" >> anno-app-turkle.html
+#echo "<script type=\"text/javascript\">" >> anno-app-turkle.html
+#cat build/static/js/main.*.js >> anno-app-turkle.html
+#echo "</script>" >> anno-app-turkle.html
 #
 echo "DONE"
